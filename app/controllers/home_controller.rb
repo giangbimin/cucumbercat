4adb96be335024a05a76859b75210b6f1dbb9a61
giangneolab <giang.nh@neo-lab.vn>
2017-06-26 16:14:16 +0700

    add friends

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,19 @@ class HomeController < ApplicationController
   before_action :require_login
 
   def index
-    @users = User.where.not('id = ?', current_user.id).order('created_at DESC')
+    @friends = User.where.not('id = ?', current_user.id).order('created_at DESC')
     @friend = current_user
     @conversations = Conversation.involving(current_user).order('created_at DESC')
     @page = 10
-    @conversation = Conversation.self_conversation(current_user.id) || Conversation.create(sender_id: current_user.id, recipient_id: current_user.id)
   end
+
+  # def load_friends
+  #   @friends = User.where.not('id = ?', current_user.id).order('created_at DESC')
+  #   @conversation = Conversation.self_conversation(current_user.id) || Conversation.create(sender_id: current_user.id, recipient_id: current_user.id)
+  # end
+
+  # def load_users
+  #   @users = User.where.not(id: User.is_friends.ids)
+  #   @conversation = Conversation.self_conversation(current_user.id) || Conversation.create(sender_id: current_user.id, recipient_id: current_user.id)
+  # end
 end
